2478126fb2ab93a3c92e929a4ae8b3557024bfae
aetos1918 <aetos.anatole@gmail.com>
2016-11-29 12:34:43 +0530

    collectstatic

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'targetibps42@gmail.com'
 EMAIL_HOST_PASSWORD = 'sarath123'
